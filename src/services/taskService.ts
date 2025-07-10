5dcb542f0c7e9bde8a0a0b78c2135fb69c9fa665
Belen Medina <belenmedinaroldan@gmail.com>
2025-07-10 02:56:33 -0300

    Se agrega llamada a la API

---
+++ src/services/taskService.ts
@@ -0,0 +1,33 @@
+import type { Task } from '../types/Task';
+
+const API_URL = import.meta.env.TASKS_API_URL;
+const headers = { 'Content-Type': 'application/json' };
+
+export async function getTasks(): Promise<Task[]> {
+    const res = await fetch(API_URL);
+    return res.json();
+}
+
+export async function createTask(task: Partial<Task>): Promise<Task> {
+	const res = await fetch(API_URL, {
+		method: 'POST',
+		headers: headers,
+		body: JSON.stringify(task),
+	});
+	return res.json();
+}
+
+export async function updateTask(id: string, task: Partial<Task>): Promise<Task> {
+	const res = await fetch(`${API_URL}/${id}`, {
+		method: 'PUT',
+		headers: headers,
+		body: JSON.stringify(task),
+	});
+	return res.json();
+}
+
+export async function deleteTask(id: string): Promise<void> {
+	await fetch(`${API_URL}/${id}`, {
+		method: 'DELETE',
+	});
+}
